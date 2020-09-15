db35b2ba22f02dc4399149821211cde89f4f534b
kingz10102 <kingz10102@gmail.com>
2020-09-15 17:34:52 -0400

    this a commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import Login from'./components/Login';
 import { getTokenFromResponse } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
 import './App.css';
 
+const spotify = new SpotifyWebApi(); 
 
 function App() {
   const [token, setToken] = useState(null);
@@ -15,6 +17,13 @@ useEffect(() => {
 
   if (_token) {
     setToken(_token)
+
+    spotify.setAccessToken(_token);
+
+    // get users account
+    spotify.getMe().then(user => {
+      console.log(user);
+    })
   }
 
   console.log("I have a token:", token);
